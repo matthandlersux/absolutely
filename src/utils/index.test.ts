a8e85aef91f9eaa094e7469d9986508c2078aa41
matt handler <72524+matthandlersux@users.noreply.github.com>
2020-10-17 15:15:20 -0400

    begin TDDing line conversion utils

---
+++ src/utils/index.test.ts
@@ -0,0 +1,109 @@
+import { convertPath, convertLine } from './index';
+
+const currentPath = '/Users/test/Documents/code/javascript/project/src/subfolder';
+
+describe('convertPath', () => {
+  it.each([
+    [
+      'file relatively from the same folder',
+      './index',
+      '/Users/test/Documents/code/javascript/project/src/subfolder/index',
+    ],
+    ['file up a directory', '../someFile', '/Users/test/Documents/code/javascript/project/src/someFile'],
+    [
+      'cousin file',
+      '../cousinFolder/someFile',
+      '/Users/test/Documents/code/javascript/project/src/cousinFolder/someFile',
+    ],
+    [
+      'path with unnecessary current file indicators',
+      './../someFile',
+      '/Users/test/Documents/code/javascript/project/src/someFile',
+    ],
+    [
+      'path with multiple unnecessary current file indicators',
+      '././../someFile',
+      '/Users/test/Documents/code/javascript/project/src/someFile',
+    ],
+    [
+      'path with unnecessary parent directory indicators',
+      '../subfolder/test',
+      '/Users/test/Documents/code/javascript/project/src/subfolder/test',
+    ],
+  ])('converts a %s', (_spec, inputPath, outputPath) => {
+    expect(convertPath(inputPath, currentPath)).toBe(outputPath);
+  });
+});
+
+describe('convertLine', () => {
+  describe('for import statements', () => {
+    it.each([
+      [
+        'default case',
+        "import * as MyThing from '../myThing';",
+        "import * as MyThing from '/Users/test/Documents/code/javascript/project/src/myThing';",
+      ],
+      [
+        'without a semicolon',
+        "import * as MyThing from '../myThing'",
+        "import * as MyThing from '/Users/test/Documents/code/javascript/project/src/myThing'",
+      ],
+      [
+        'using double quotes',
+        'import * as MyThing from "../myThing";',
+        'import * as MyThing from "/Users/test/Documents/code/javascript/project/src/myThing";',
+      ],
+    ])('%s', (_spec, line, expected) => {
+      expect(convertLine(line, currentPath)).toEqual(expected);
+    });
+  });
+
+  describe('converts a line for require statements', () => {
+    it.each([
+      [
+        'default case',
+        "const library = require('../myThing');",
+        "const library = require('/Users/test/Documents/code/javascript/project/src/myThing');",
+      ],
+      [
+        'appearing in some code (indented)',
+        "  const library = require('../myThing');",
+        "  const library = require('/Users/test/Documents/code/javascript/project/src/myThing');",
+      ],
+      [
+        'without assignment',
+        "require('../myThing');",
+        "require('/Users/test/Documents/code/javascript/project/src/myThing');",
+      ],
+      [
+        'with a let assignment',
+        "let library = require('../myThing');",
+        "let library = require('/Users/test/Documents/code/javascript/project/src/myThing');",
+      ],
+      [
+        'with a var assignment',
+        "var library = require('../myThing');",
+        "var library = require('/Users/test/Documents/code/javascript/project/src/myThing');",
+      ],
+      [
+        'without a semicolon',
+        "const library = require('../myThing')",
+        "const library = require('/Users/test/Documents/code/javascript/project/src/myThing')",
+      ],
+      [
+        'using double quotes',
+        'const library = require("../myThing");',
+        'const library = require("/Users/test/Documents/code/javascript/project/src/myThing");',
+      ],
+    ])('%s', (_spec, line, expected) => {
+      expect(convertLine(line, currentPath)).toEqual(expected);
+    });
+  });
+
+  it.each([['that is just code baby', 'const value = someFunction(call);']])(
+    'does not edit a line %s',
+    (_description, line) => {
+      expect(convertLine(line, currentPath)).toEqual(line);
+    },
+  );
+});
---
+++ src/utils/index.ts
@@ -0,0 +1,66 @@
+import { take, drop } from 'lodash';
+
+type PathSpec = {
+  upDirectoryCount: number;
+  topDownRelativePathPieces: Array<string>;
+};
+
+const importRegex = /^import .* from ('(.*)'|"(.*)");?$/i;
+const requireRegex = /\brequire\(('(.*)'|"(.*)")\);?/i;
+
+export const convertLine = (currentLine: string, currentPath: string): string => {
+  return (
+    matchLineAndReplace(currentLine, importRegex, 1, currentPath) ||
+    matchLineAndReplace(currentLine, requireRegex, 1, currentPath) ||
+    currentLine
+  );
+};
+
+const matchLineAndReplace = (line: string, regex: RegExp, groupOffsetIndex: number, currentPath: string) => {
+  const requireResult = line.match(regex);
+
+  if (requireResult != null) {
+    const quoted = requireResult[groupOffsetIndex];
+    const unquoted = requireResult[groupOffsetIndex + 1] || requireResult[groupOffsetIndex + 2];
+
+    if (quoted && unquoted) {
+      const quoteChar = quoted[0];
+      return line.replace(quoted, `${quoteChar}${convertPath(unquoted, currentPath)}${quoteChar}`);
+    } else return null;
+  } else return null;
+};
+
+export const convertPath = (relativePath: string, currentPath: string): string => {
+  const currentPathPieces: Array<string> = currentPath.split('/');
+  const relativePieces: Array<string> = relativePath.split('/');
+  const pathSpec: PathSpec = pathSpecFor(relativePath);
+  return [
+    ...take(currentPathPieces, currentPathPieces.length - pathSpec.upDirectoryCount),
+    ...pathSpec.topDownRelativePathPieces,
+  ].join('/');
+};
+
+const pathSpecFor = (relativePath: string): PathSpec => {
+  return relativePath.split('/').reduce<PathSpec>(
+    (acc, part) => {
+      switch (part) {
+        case '..':
+          return {
+            ...acc,
+            upDirectoryCount: acc.upDirectoryCount + 1,
+          };
+        case '.':
+          return acc;
+        default:
+          return {
+            ...acc,
+            topDownRelativePathPieces: [...acc.topDownRelativePathPieces, part],
+          };
+      }
+    },
+    {
+      upDirectoryCount: 0,
+      topDownRelativePathPieces: [],
+    },
+  );
+};
